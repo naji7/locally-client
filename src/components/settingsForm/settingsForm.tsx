4d7177bd514c8ddf9b15aec67e1a22eeb38437b3
CL-tharushi <tharushi@codinglegends.io>
2024-04-03 17:10:47 +0530

    setting page functions

--- src/components/settingsForm/settingsForm.tsx
+++ src/components/settingsForm/settingsForm.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Withdraw from "./withdraw";
 import Link from "next/link";
+import { useForm } from 'react-hook-form';
+
 
 function SettingsForm() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data); 
+  }
+  
+
   return (
     <div className="flex flex-col items-center  pl-[30px]  xl:pl-[16.75rem] pr-[96px] py-[2rem] w-full">
       <Withdraw />
-      <form className="flex flex-col items-center justify-between w-full gap-8">
+      <form action="" className="flex flex-col items-center justify-between w-full gap-8"onSubmit={handleSubmit(onSubmit)} >
         <div className="flex flex-row items-center justify-center w-full gap-6">
           <div className="flex flex-col  w-full gap-[5px]">
             <label className="ml-3 text-sm font-normal text-[#6B6B6B] opacity-40">
@@ -16,6 +26,7 @@ function SettingsForm() {
               id="fname"
               type="text"
               placeholder="Sean "
+              {...register("firstName")}
               className="border rounded-xl w-full py-[16px] px-[24px] text-black placeholder-black text-base font-medium"
             />
           </div>
@@ -39,8 +50,10 @@ function SettingsForm() {
             id="email"
             type="email"
             placeholder="sean.thernards@gmail.com "
+            {...register("email", { required: true })}
             className="border rounded-xl w-full py-[16px] px-[24px] text-black placeholder-black text-base font-medium"
           />
+          {errors.email && <span className="text-red-500">Email is required</span>}
         </div>
 
         <div className="flex flex-row items-center justify-center w-full gap-6">
@@ -130,12 +143,13 @@ function SettingsForm() {
           <input
             type="checkbox"
             id="checkbox"
+            {...register("agreeTerms")}
             className="form-checkbox h-5 w-5 text-[#FF4C00]"
           />
           <h3 className="ml-2 text-gray-700 text-sm font-normal">I agree with the <span className="text-[#FF4C00]">Terms of use</span></h3>
         </div>
         <Link className="w-1/6" href="#">
-        <button className="border rounded-xl bg-[#FF4C00] text-white w-full py-[21px] px-[24px]">
+        <button type="submit" className="border rounded-xl bg-[#FF4C00] text-white w-full py-[21px] px-[24px]">
           Confirm
         </button>
         </Link>
